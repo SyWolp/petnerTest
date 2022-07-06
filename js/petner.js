7fc7a02a5665176b836363961a09f90be0691701
soohong <soohongkim@soohongui-MacBookPro.local>
2022-07-06 18:40:42 +0900

    v2

--- js/petner.js
+++ js/petner.js
@@ -4,14 +4,63 @@ function getTitle(){
 }
 
 (async function main() { 
-  let text;
-  let a = [];
+  let info;
+  let mainInfo = [];
+  let contentBox = document.querySelector(".contentBox");
+  let leftButton = document.querySelector(".leftButton");
+  let rightButton = document.querySelector(".rightButton");
   try {
-    text = await getTitle();
-    a.push(...text);
+    info = await getTitle();
+    mainInfo.push(...info);
   }
   catch(error){
     console.log(error);
   }
-  console.log(text);
+  console.log(mainInfo);
+  let infoPlusHTML = [];
+  for(let i = 0; i < mainInfo.length; i++) {
+    infoPlusHTML.push(`
+    <div class="petnerContent">
+      <div class="contentHeader">
+        <div class="contentHeaderLeft">
+          <img class="petners" src="${mainInfo[i].petner.image}" alt="펫시터님">
+        </div>
+        <div class="contentHeaderRight">
+          <h5 class="petnerName">${mainInfo[i].petner.name}<h5>
+          <div>😀 전문펫시터</div>
+        </div>
+      </div>
+      <div class="cutePet">
+        <img class="cutePetImg" src="${mainInfo[i].image}" alt="반려동물들">
+      </div>
+      <footer class="contentFooter">
+        <div class="times">~시간 전</div>
+        <div class="petName">${mainInfo[i].companion.name}</div>
+      </footer>
+    </div>`);
+  }
+  for(let i = 0; i < infoPlusHTML.length; i++) {
+    contentBox.innerHTML += infoPlusHTML[i];
+  }
+  let clickWidth = 0;
+  rightButton.addEventListener("click",()=>{
+    if(clickWidth <= -1280) {
+      clickWidth = 0;
+      contentBox.style.transform = `translate(${clickWidth},0)`;
+    }else {
+      clickWidth = clickWidth - 128;
+      contentBox.style.transform = `translate(${clickWidth}px,0)`;
+    }
+  })
+  leftButton.addEventListener("click",()=>{
+    console.log(clickWidth)
+    if(clickWidth !== 0) {
+      clickWidth = clickWidth + 128;
+      contentBox.style.transform = `translate(${clickWidth}px,0)`;
+    }else {
+      clickWidth = -1280;
+      contentBox.style.transform = `translate(${clickWidth}px,0)`;
+    }
+  })
+  
 })();
